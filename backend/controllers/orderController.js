bfac1df35f0489438de3a02b885be52186218716
ymizrahi <yitzhak.tech@gmail.com>
2021-01-04 19:46:24 +0200

    created controller and route to get logged in user orders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -79,4 +79,12 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   }
 })
 
-export { addorderItems, getOrderById, updateOrderToPaid }
+// @desc    Get logged in user orders
+// @route   GET /api/orders/myorders
+// @access  Private
+const getMyOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id })
+  res.json(orders)
+})
+
+export { addorderItems, getOrderById, updateOrderToPaid, getMyOrders }
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,12 +2,14 @@ import express from 'express'
 const router = express.Router()
 import {
   addorderItems,
+  getMyOrders,
   getOrderById,
   updateOrderToPaid,
 } from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').post(protect, addorderItems)
+router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
 
