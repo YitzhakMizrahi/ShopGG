fb34689caa4106b8359796721d924a60b2eac37d
ymizrahi <yitzhak.tech@gmail.com>
2021-01-05 01:53:10 +0200

    creating product actions, constants and reducers to delete a product as admin user

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,15 +1,18 @@
 import {
-  PRODUCT_LIST_REQUREST,
+  PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
-  PRODUCT_DETAILS_REQUREST,
+  PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
   PRODUCT_DETAILS_FAIL,
+  PRODUCT_DELETE_REQUEST,
+  PRODUCT_DELETE_SUCCESS,
+  PRODUCT_DELETE_FAIL,
 } from '../constants/productConstants'
 
 export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
-    case PRODUCT_LIST_REQUREST:
+    case PRODUCT_LIST_REQUEST:
       return { loading: true, products: [] }
     case PRODUCT_LIST_SUCCESS:
       return { loading: false, products: action.payload }
@@ -25,11 +28,24 @@ export const productDetailsReducer = (
   action
 ) => {
   switch (action.type) {
-    case PRODUCT_DETAILS_REQUREST:
+    case PRODUCT_DETAILS_REQUEST:
       return { loading: true, ...state }
     case PRODUCT_DETAILS_SUCCESS:
       return { loading: false, product: action.payload }
     case PRODUCT_DETAILS_FAIL:
+      return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
+
+export const productDeleteReducer = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_DELETE_REQUEST:
+      return { loading: true }
+    case PRODUCT_DELETE_SUCCESS:
+      return { loading: false, success: true }
+    case PRODUCT_DELETE_FAIL:
       return { loading: false, error: action.payload }
     default:
       return state
