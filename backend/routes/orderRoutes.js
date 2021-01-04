7f3eeadea0c1feeb6387845a72098a8bc960eab4
ymizrahi <yitzhak.tech@gmail.com>
2021-01-04 17:31:43 +0200

    created orderActions, orderConstants and orderReducers / modified orderController and orderRoutes to connect with frontend

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
 const router = express.Router()
-import { addorderItems } from '../controllers/orderController.js'
+import { addorderItems, getOrderById } from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').post(protect, addorderItems)
+router.route('/:id').get(protect, getOrderById)
 
 export default router
