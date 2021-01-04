6bc9242d23bb5cb84c3804d7f28d157b81770006
ymizrahi <yitzhak.tech@gmail.com>
2021-01-04 22:10:43 +0200

    created end points for getting a user by its Id and updating a user by its Id

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,6 +4,8 @@ import {
   authUser,
   registerUser,
   getUserProfile,
+  getUserById,
+  updateUser,
   updateUserProfile,
   getUsers,
   deleteUser,
@@ -16,6 +18,10 @@ router
   .route('/profile')
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile)
-router.route('/:id').delete(protect, admin, deleteUser)
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser)
 
 export default router
