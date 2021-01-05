609c4f2bc71c4b28943e36e2a0dfca66cc580e72
ymizrahi <yitzhak.tech@gmail.com>
2021-01-05 16:20:42 +0200

    creating order actions, constants and reducers to list orders as admin & creating OrderListScreen to view them

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import UserListScreen from './pages/UserListScreen'
 import UserEditScreen from './pages/UserEditScreen'
 import ProductListScreen from './pages/ProductListScreen'
 import ProductEditScreen from './pages/ProductEditScreen'
+import OrderListScreen from './pages/OrderListScreen'
 
 const App = () => {
   return (
@@ -37,6 +38,7 @@ const App = () => {
           <Route path='/admin/user/:id/edit' component={UserEditScreen} />
           <Route path='/admin/productlist' component={ProductListScreen} />
           <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
+          <Route path='/admin/orderlist' component={OrderListScreen} />
           <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
